a3c9766da5051c4eeebefdea644f59d0045684c1
246John <johnunnamed@gmail.com>
2023-09-27 13:27:48 +0300

    Added solution for Task15

--- Program.cs
+++ Program.cs
@@ -6,17 +6,17 @@ Console.Clear();
 // вход трёхзначное число и на выходе показывает вторую
 // цифру этого числа.
 
-// Console.WriteLine("Введите трёхзначное число: ");
-// int Number = Convert.ToInt32(Console.ReadLine());
-// int N2 = Number / 10 % 10;
-// if ((Number < 1000 && Number > 99) || (Number > -1000 && Number < -99))
-// {
-//     Console.WriteLine($"Ввод: {Number}, результат: {N2}");
-// }
-// else
-// {
-//     Console.WriteLine("Число не трёхзначное!");
-// }
+Console.WriteLine("Введите трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int N2 = Number / 10 % 10;
+if ((Number < 1000 && Number > 99) || (Number > -1000 && Number < -99))
+{
+    Console.WriteLine($"Ввод: {Number}, результат: {N2}");
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное!");
+}
 
 //-----------------------------------------------------------------------
 
@@ -25,9 +25,9 @@ Console.Clear();
 // цифры нет.
 
 Console.WriteLine("Введите число: ");
-int Number = Convert.ToInt32(Console.ReadLine());
+Number = Convert.ToInt32(Console.ReadLine());
 
-if (Number < 0) {Number = Number * (-1);}
+if (Number < 0) {Number *= -1;}
 
 if (Number < 100)
 {
@@ -35,13 +35,35 @@ if (Number < 100)
 }
 else if (Number >=100 && Number <= 999)
 {
-    Console.WriteLine($"Третья цифра числа: {Number % 10 % 10}");
+    Console.WriteLine($"Третья цифра числа: {Number % 10}");
 }
 else if (Number > 999)
 {
     while (Number > 999)
     {
-        Number = Number / 10;   
+        Number /= 10;   
     }
-    Console.WriteLine($"Третья цифра числа: {Number % 10 % 10}");
+    Console.WriteLine($"Третья цифра числа: {Number % 10}");
+}
+
+//-----------------------------------------------------------------------
+
+// "Задача 15": Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+Console.WriteLine("Введите цифру дня недели: ");
+int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+if (DayOfWeek > 7 || DayOfWeek < 1)
+{
+    Console.WriteLine("Введите корректное значение! (1-7)");
+}
+else if (DayOfWeek == 6 || DayOfWeek == 7)
+{
+    Console.WriteLine("Выходной день! ^.^");
+}
+else
+{
+    Console.WriteLine("Будний день. :(");
 }
