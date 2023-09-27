98f612a89f94fea6d11d60bee9879956bc8e71b0
246John <johnunnamed@gmail.com>
2023-09-27 12:16:16 +0300

    Added solution for Task10

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Globalization;
+
+Console.Clear();
+
+// "Задача 10": Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int N2 = Number / 10 % 10;
+if ((Number < 1000 && Number > 99) || (Number > -1000 && Number < -99))
+{
+    Console.WriteLine($"Ввод: {Number}, результат: {N2}");
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное!");
+}
