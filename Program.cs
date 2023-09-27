c95fd75c35ba65a9665766710b54a792aafa54c8
246John <johnunnamed@gmail.com>
2023-09-27 13:12:45 +0300

    Added solution for Task13

--- Program.cs
+++ Program.cs
@@ -6,14 +6,42 @@ Console.Clear();
 // вход трёхзначное число и на выходе показывает вторую
 // цифру этого числа.
 
-Console.WriteLine("Введите трёхзначное число: ");
+// Console.WriteLine("Введите трёхзначное число: ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// int N2 = Number / 10 % 10;
+// if ((Number < 1000 && Number > 99) || (Number > -1000 && Number < -99))
+// {
+//     Console.WriteLine($"Ввод: {Number}, результат: {N2}");
+// }
+// else
+// {
+//     Console.WriteLine("Число не трёхзначное!");
+// }
+
+//-----------------------------------------------------------------------
+
+// "Задача 13": Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+
+Console.WriteLine("Введите число: ");
 int Number = Convert.ToInt32(Console.ReadLine());
-int N2 = Number / 10 % 10;
-if ((Number < 1000 && Number > 99) || (Number > -1000 && Number < -99))
+
+if (Number < 0) {Number = Number * (-1);}
+
+if (Number < 100)
 {
-    Console.WriteLine($"Ввод: {Number}, результат: {N2}");
+    Console.WriteLine("Третьей цифры нет");
 }
-else
+else if (Number >=100 && Number <= 999)
 {
-    Console.WriteLine("Число не трёхзначное!");
+    Console.WriteLine($"Третья цифра числа: {Number % 10 % 10}");
+}
+else if (Number > 999)
+{
+    while (Number > 999)
+    {
+        Number = Number / 10;   
+    }
+    Console.WriteLine($"Третья цифра числа: {Number % 10 % 10}");
 }
